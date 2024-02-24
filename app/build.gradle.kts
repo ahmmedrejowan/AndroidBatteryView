3b57f1630eb73b95fcde3095811b86b1089e59fc
ahmmedrejowan <ahmmedrejowan@gmail.com>
2024-02-24 18:33:46 +0600

    Dependency update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
 
 dependencies {
 
-    implementation(project(":abv"))
+   // implementation(project(":abv"))
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -53,5 +53,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.github.ahmmedrejowan:AndroidBatteryView:0.1")
+
 }
 
